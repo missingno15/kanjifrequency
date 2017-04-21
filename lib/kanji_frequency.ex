599fb78e6a1dc37e826aa0a1e31c91c976696594
Kenneth Uy <missingno15@gmail.com>
2017-04-21 04:19:12 -0400

    Initial commit

---
+++ lib/kanji_frequency.ex
@@ -0,0 +1,18 @@
+defmodule KanjiFrequency do
+  @moduledoc """
+  Documentation for KanjiFrequency.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KanjiFrequency.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kanji_frequency_test.exs
@@ -0,0 +1,8 @@
+defmodule KanjiFrequencyTest do
+  use ExUnit.Case
+  doctest KanjiFrequency
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
